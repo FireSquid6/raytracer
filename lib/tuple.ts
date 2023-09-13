449f0d0607099ac8c0809bc73f04e2b7a931a9ad
firesquid <jdeiss06@gmail.com>
2023-09-12 22:36:22 -0500

    add tuple operations

--- lib/tuple.ts
+++ lib/tuple.ts
@@ -5,6 +5,8 @@ export interface Tuple {
   w: number;
 }
 
+export const ZERO = tuple(0, 0, 0, 0);
+
 export function tuple(x: number, y: number, z: number, w: number): Tuple {
   return {
     x: x,
@@ -28,4 +30,48 @@ export function add(a: Tuple, b: Tuple) {
 
 export function subtract(a: Tuple, b: Tuple) {
   return tuple(a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w);
+}
+
+export function invert(a: Tuple) {
+  return tuple(-a.x, -a.y, -a.z, -a.w);
+}
+
+export function scalarMultiply(a: Tuple, b: number) {
+  return tuple(a.x * b, a.y * b, a.z * b, a.w * b);
+}
+
+export function scalarDivide(a: Tuple, b: number) {
+  return tuple(a.x / b, a.y / b, a.z / b, a.w / b);
+}
+
+export function magnitude(a: Tuple) {
+  if (a.w === 1) {
+    throw "cannot calculate magnitude of a point";
+  }
+
+  return Math.sqrt(a.x ** 2 + a.y ** 2 + a.z ** 2);
+}
+
+export function normalized(a: Tuple) {
+  return scalarDivide(a, magnitude(a));
+}
+
+export function dotProduct(a: Tuple, b: Tuple) {
+  if (a.w === 1 || b.w === 1) {
+    throw "cannot calculate dot product of a point";
+  }
+
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+export function crossProduct(a: Tuple, b: Tuple) {
+  if (a.w === 1 || b.w === 1) {
+    throw "cannot calculate cross product of a point";
+  }
+
+  return vector(
+    a.y * b.z - a.z * b.y,
+    a.z * b.x - a.x * b.z,
+    a.x * b.y - a.y * b.x
+  );
 }
--- tests/tuple.test.ts
+++ tests/tuple.test.ts
@@ -1,5 +1,18 @@
-import { test, expect } from "bun:test";
-import { tuple, point, vector, add, subtract } from "../lib/tuple";
+import { test, expect, describe } from "bun:test";
+import {
+  tuple,
+  invert,
+  crossProduct,
+  dotProduct,
+  point,
+  vector,
+  add,
+  subtract,
+  scalarMultiply,
+  scalarDivide,
+  normalized,
+  magnitude,
+} from "../lib/tuple";
 
 test("tuple", () => {
   const t = tuple(1, 2, 3, 4);
@@ -45,4 +58,90 @@ test("subtract tuple", () => {
   expect(sum.y).toEqual(0);
   expect(sum.z).toEqual(0);
   expect(sum.w).toEqual(0);
+});
+
+test("invert tuple", () => {
+  const a = tuple(1, 2, 3, 1);
+  const b = invert(a);
+  expect(b.x).toEqual(-1);
+  expect(b.y).toEqual(-2);
+  expect(b.z).toEqual(-3);
+  expect(b.w).toEqual(-1);
+});
+
+test("scalar multiply tuple", () => {
+  const a = tuple(1, 2, 3, 1);
+  const b = 2;
+  const c = scalarMultiply(a, b);
+  expect(c.x).toEqual(2);
+  expect(c.y).toEqual(4);
+  expect(c.z).toEqual(6);
+  expect(c.w).toEqual(2);
+});
+
+test("scalar divide tuple", () => {
+  const a = tuple(1, 2, 3, 1);
+  const b = 2;
+  const c = scalarDivide(a, b);
+  expect(c.x).toEqual(0.5);
+  expect(c.y).toEqual(1);
+  expect(c.z).toEqual(1.5);
+  expect(c.w).toEqual(0.5);
+});
+
+describe("magnitude of vector", () => {
+  test("returns the magnitude", () => {
+    const a = vector(1, 2, 3);
+    const b = magnitude(a);
+    expect(b).toEqual(Math.sqrt(14));
+  });
+
+  test("throws an error if the tuple is a point", () => {
+    const a = point(1, 2, 3);
+    expect(() => magnitude(a)).toThrow("cannot calculate magnitude of a point");
+  });
+});
+
+test("vector is normalized", () => {
+  const a = vector(1, 2, 3);
+  const b = normalized(a);
+  expect(magnitude(b)).toEqual(1);
+});
+
+describe("dot product", () => {
+  test("returns the dot product", () => {
+    const a = vector(1, 2, 3);
+    const b = vector(2, 3, 4);
+
+    const c = dotProduct(a, b);
+    expect(c).toEqual(20);
+  });
+
+  test("throws an error if the tuple is a point", () => {
+    const a = point(1, 2, 3);
+    const b = vector(2, 3, 4);
+
+    expect(() => dotProduct(a, b)).toThrow(
+      "cannot calculate dot product of a point"
+    );
+  });
+});
+
+describe("cross product", () => {
+  test("returns the cross product", () => {
+    const a = vector(1, 2, 3);
+    const b = vector(2, 3, 4);
+
+    const c = crossProduct(a, b);
+    expect(c).toEqual(vector(-1, 2, -1));
+  });
+
+  test("throws an error if the tuple is a point", () => {
+    const a = point(1, 2, 3);
+    const b = vector(2, 3, 4);
+
+    expect(() => crossProduct(a, b)).toThrow(
+      "cannot calculate cross product of a point"
+    );
+  });
 });
