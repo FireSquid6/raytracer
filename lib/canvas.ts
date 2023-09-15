4ebe44d00d6e7c6b70a08197aa965413578e346a
firesquid <jdeiss06@gmail.com>
2023-09-14 20:56:59 -0500

    add canvas module

--- lib/canvas.ts
+++ lib/canvas.ts
@@ -7,3 +7,75 @@ export interface Color {
 export function color(r: number, g: number, b: number): Color {
   return { r, g, b };
 }
+
+export function add(a: Color, b: Color) {
+  return color(a.r + b.r, a.g + b.g, a.b + b.b);
+}
+
+export function subtract(a: Color, b: Color) {
+  return color(a.r - b.r, a.g - b.g, a.b - b.b);
+}
+
+export function scalarMultiply(a: Color, b: number) {
+  return color(a.r * b, a.g * b, a.b * b);
+}
+
+export function schurProduct(a: Color, b: Color) {
+  return color(a.r * b.r, a.g * b.g, a.b * b.b);
+}
+
+export function colorEquals(a: Color, b: Color) {
+  return a.r === b.r && a.g === b.g && a.b === b.b;
+}
+
+export class Canvas {
+  width = 0;
+  height = 0;
+  pixels: Color[][] = [];
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+
+    for (let i = 0; i < width; i++) {
+      this.pixels[i] = [];
+      for (let j = 0; j < height; j++) {
+        this.pixels[i][j] = color(0, 0, 0);
+      }
+    }
+  }
+
+  setPixel(x: number, y: number, color: Color) {
+    this.pixels[x][y] = color;
+  }
+
+  getPixel(x: number, y: number) {
+    return this.pixels[x][y];
+  }
+
+  toPPM() {
+    let ppm = `P3\n${this.width} ${this.height}\n255\n`;
+
+    for (let j = 0; j < this.height; j++) {
+      let line = "";
+      for (let i = 0; i < this.width; i++) {
+        const pixel = this.pixels[i][j];
+        const r = Math.round(pixel.r * 255);
+        const g = Math.round(pixel.g * 255);
+        const b = Math.round(pixel.b * 255);
+        line += `${r} ${g} ${b} `;
+      }
+      ppm += line + "\n";
+    }
+
+    return ppm;
+  }
+
+  toPng() {
+    // todo
+  }
+
+  toJpeg() {
+    // todo
+  }
+}
--- tests/canvas.test.ts
+++ tests/canvas.test.ts
@@ -1,9 +1,84 @@
-import { color } from "../lib/canvas";
-import { test, expect } from "bun:test";
+import * as Canvas from "../lib/canvas";
+import { test, describe, expect } from "bun:test";
 
-test("color", () => {
-  const c = color(-0.5, 0.4, 1.7);
-  expect(c.r).toEqual(-0.5);
-  expect(c.g).toEqual(0.4);
-  expect(c.b).toEqual(1.7);
+describe("Colors", () => {
+  test("color", () => {
+    const c = Canvas.color(-0.5, 0.4, 1.7);
+    expect(c.r).toEqual(-0.5);
+    expect(c.g).toEqual(0.4);
+    expect(c.b).toEqual(1.7);
+  });
+
+  test("add color", () => {
+    const a = Canvas.color(0.9, 0.6, 0.75);
+    const b = Canvas.color(0.7, 0.1, 0.25);
+
+    const sum = Canvas.add(a, b);
+    expect(sum.r).toBeCloseTo(1.6);
+    expect(sum.g).toBeCloseTo(0.7);
+    expect(sum.b).toBeCloseTo(1.0);
+  });
+
+  test("subtract color", () => {
+    const a = Canvas.color(0.9, 0.6, 0.75);
+    const b = Canvas.color(0.7, 0.1, 0.25);
+
+    const sum = Canvas.subtract(a, b);
+    expect(sum.r).toBeCloseTo(0.2);
+    expect(sum.g).toBeCloseTo(0.5);
+    expect(sum.b).toBeCloseTo(0.5);
+  });
+
+  test("scalar multiply color", () => {
+    const a = Canvas.color(0.2, 0.3, 0.4);
+
+    const sum = Canvas.scalarMultiply(a, 2);
+    expect(sum.r).toBeCloseTo(0.4);
+    expect(sum.g).toBeCloseTo(0.6);
+    expect(sum.b).toBeCloseTo(0.8);
+  });
+
+  test("schur product color", () => {
+    const a = Canvas.color(1, 0.2, 0.4);
+    const b = Canvas.color(0.9, 1, 0.1);
+
+    const sum = Canvas.schurProduct(a, b);
+    expect(sum.r).toBeCloseTo(0.9);
+    expect(sum.g).toBeCloseTo(0.2);
+    expect(sum.b).toBeCloseTo(0.04);
+  });
+
+  test("color equals", () => {
+    const a = Canvas.color(1, 0.2, 0.4);
+    const b = Canvas.color(1, 0.2, 0.4);
+    const c = Canvas.color(0.9, 1, 0.1);
+
+    expect(Canvas.colorEquals(a, b)).toEqual(true);
+    expect(Canvas.colorEquals(a, c)).toEqual(false);
+  });
+});
+
+describe("Canvas", () => {
+  test("create canvas", () => {
+    const c = new Canvas.Canvas(10, 20);
+    expect(c.width).toEqual(10);
+    expect(c.height).toEqual(20);
+    expect(c.pixels.length).toEqual(10);
+    expect(c.pixels[0].length).toEqual(20);
+    expect(c.pixels[0][0]).toEqual(Canvas.color(0, 0, 0));
+  });
+
+  test("write pixel to canvas", () => {
+    const c = new Canvas.Canvas(10, 20);
+    const red = Canvas.color(1, 0, 0);
+    c.setPixel(2, 3, red);
+    expect(c.pixels[2][3]).toEqual(red);
+  });
+
+  test("get pixel from canvas", () => {
+    const c = new Canvas.Canvas(10, 20);
+    const red = Canvas.color(1, 0, 0);
+    c.setPixel(2, 3, red);
+    expect(c.getPixel(2, 3)).toEqual(red);
+  });
 });
